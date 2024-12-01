8595d6f2be402f10556754fc95fe44e9d46e33d6
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2024-12-01 17:31:55 +0100

    Bump version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * GitHub Action for comparing code coverage between branches
  * Supports Java, JavaScript, and Python coverage formats
- * Version: 0.0.16
+ * Version: 0.0.18
  */
 const core = require('@actions/core');
 const github = require('@actions/github');
